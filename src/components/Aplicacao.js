b58dbd85821ef5c2277128c5223f120b33bba4fc
Vinicius Predebon <kadiobaoff@gmail.com>
2023-03-04 18:17:19 -0300

    Atualizacao no layout e componentes

--- src/components/Aplicacao.js
+++ src/components/Aplicacao.js
@@ -1,9 +1,16 @@
+import Deck from "./Deck";
+
 import styled from "styled-components";
+import logo from "../assets/logo.png"
 
 export default function Aplicacao() {
     return (
         <TelaPrincipal>
-            a
+            <Logo>
+                <img src={logo} alt="" />
+                <h1>ZapRecall</h1>
+            </Logo>
+            <Deck />
         </TelaPrincipal>
     )
 }
@@ -12,4 +19,27 @@ const TelaPrincipal = styled.div`
     height: 90vh;
     width: 100vw;
     background-color: #FB6B6B;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const Logo = styled.div`
+    width: 255px;
+    height: 60px; 
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    img{
+        width: 52px;
+        height: 60px;
+    }
+    h1{
+        font-family: 'Righteous';
+        font-weight: 400;
+        font-size: 36px;
+        text-align: center;
+        color: #FFFFFF;
+        width: 200px;
+    }
 `
---
+++ src/components/Card.js
@@ -0,0 +1,106 @@
+import styled from "styled-components"
+import setaPlay from "../assets/seta_play.png"
+import setaVirar from "../assets/seta_virar.png"
+
+export default function Card() {
+    return (
+        <Cartao>
+            <CartaoFechado>
+                <h1>Perguntaa 2</h1>
+                <img src={setaPlay} alt="" />
+            </CartaoFechado>
+            <Pergunta>
+                <h1>O que é JSX?</h1>
+                <img src={setaVirar} alt="" />
+            </Pergunta>
+            <Resposta>
+                <h1>JSX é uma sintaxe para
+                    escrever HTML dentro do JS</h1>
+                <div>
+                    <button>Não lembrei</button>
+                    <button>Quase não lembrei</button>
+                    <button>Zap!</button>
+                </div>
+            </Resposta>
+        </Cartao>
+    )
+}
+
+const Cartao = styled.div`
+    width: 80vw;
+    background-color: #FFFFFF;
+    box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+    border-radius: 5px;
+    padding: 15px;
+`
+
+const CartaoFechado = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    display: none;
+    h1{
+        font-family: 'Recursive';
+        font-weight: 700;
+        font-size: 16px;
+        color: #333333;
+    }
+    img{
+        width: 20px;
+        height: 23px;
+    }
+`
+
+const Pergunta = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    display: none;
+    img{
+        width: 30px;
+        height: 20px;
+        align-self: flex-end;
+    }
+    h1{
+        font-family: 'Recursive';
+        font-weight: 400;
+        font-size: 18px;
+        line-height: 22px;
+        color: #333333;
+        height: 87px;
+    }
+`
+
+const Resposta = styled.div`
+    h1{
+        font-family: 'Recursive';
+        font-weight: 400;
+        font-size: 18px;
+        line-height: 22px;
+        color: #333333;
+        height: 65px;
+    }
+    div{
+        display: flex;
+        justify-content: space-between;
+    }
+    button{
+        width: 85px;
+        height: 37px;
+        border-radius: 5px;
+        border: none;
+        color: #FFFFFF;
+        font-family: 'Recursive';
+        font-weight: 400;
+        font-size: 12px;
+    }
+    button:first-child{
+        background-color: #FF3030;
+    }
+    button:nth-child(2){
+        background-color: #FF922E;
+    }
+    button:nth-child(3){
+        background-color: #2FBE34;
+    }
+`
